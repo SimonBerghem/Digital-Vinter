24c3ac0638989bc07d9fb5d3098625ce97aa9890
Samuel Gradén <samuelgraden@pub108-182.pub.ltu.se>
2020-02-26 16:46:12 +0100

    Issue 43

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,18 +8,23 @@ extern crate mysql;
 use std::{thread, time::{Duration}};
 use mysql::chrono::{Local};
 //NYTT
+use reqwest::ClientBuilder;
 use reqwest::header::USER_AGENT;
-
+use reqwest::header::CONTENT_TYPE;
+use std::io;
+use std::fs::File;
+//use reqwest::blocking;
 mod auth;
 mod fetch;
 mod parse_xml;
 mod database;
 
 
+
 fn main() {
 
     testPost();
-
+    //testPost_2();
     let opts = database::get_opts(auth::USER_DB, auth::PASS_DB, auth::ADDR_DB, auth::NAME_DB);
     // // // Create new pool connections 
     let pool = mysql::Pool::new(opts).expect("Pool failed to get opts!");
@@ -93,38 +98,58 @@ fn main() {
 
     }
 
-    
-
-
-
-   
-
 
 }
 
 
+
 fn testPost(){
-    println!("Hello, world");
+
 
   //<LOGIN authenticationkey="{AUTH}" />´
-    let client = reqwest::Client::new();
-    let res = client.post("https://api.trafikinfo.trafikverket.se/v2/data.xml")
-    .header(USER_AGENT,"HalladiGOing")
-    .body("<REQUEST>
-    <LOGIN authenticationkey=\"d8b542b2dafe40f999f223c7aff04046\" />
-    <QUERY objecttype=\"Situation\" schemaversion=\"1.2\">
-          <FILTER>
+    
+    let client = reqwest::blocking::Client::new();
+  //  .user_agent(APP_USER_AGENT)
+    //.build();POST /v2/data.xml HTTP/1.1
+  /*  Content-Type: text/xml
+    User-Agent: TEST
+    Accept: **
+    Cache-Control: no-cache
+    Host: api.trafikinfo.trafikverket.se
+    Content-Length: 433
+    Connection: keep-alive*/
+    let mut res = client.post("https://api.trafikinfo.trafikverket.se/v2/data.xml").header(USER_AGENT,"DATAEXLTU20").header(CONTENT_TYPE,"text/xml")
+    .body("
+    <REQUEST>
+        <LOGIN authenticationkey=\"d8b542b2dafe40f999f223c7aff04046\" />
+        <QUERY objecttype=\"Situation\" schemaversion=\"1.2\">
+            <FILTER>
                 <EQ name=\"Deviation.MessageType\" value=\"Olycka\" />
-          </FILTER>
-          <INCLUDE>Deviation.Id</INCLUDE>
-          <INCLUDE>Deviation.Header</INCLUDE>
-          <INCLUDE>Deviation.IconId</INCLUDE>
-          <INCLUDE>Deviation.Geometry.WGS84</INCLUDE>
-    </QUERY>
-</REQUEST>")
+            </FILTER>
+            <INCLUDE>Deviation.Id</INCLUDE>
+            <INCLUDE>Deviation.Header</INCLUDE>
+            <INCLUDE>Deviation.IconId</INCLUDE>
+            <INCLUDE>Deviation.Geometry.WGS84</INCLUDE>
+        </QUERY>
+    </REQUEST>")
     .send()
-    .expect("Connection något, jag vet inte");
+    .unwrap();
    
 println!("Status: {}",res.status());
-println!("Headers:\n{:#?}", res.headers());
+let mut file = File::create("TESTFILE.txt")
+        .expect("Error creating file, station_data");
+io::copy(&mut res, &mut file)
+    .expect("Failed to read response to file");
+//println!("Status: {}",res.text());
+//println!("Headers:\n{}", res.headers());
+
+let c = reqwest::blocking::Client::new();
+let res = c.get("https://rust-lang.org").send().unwrap();
+    println!("Status: {}", res.status());
+
+ 
+    
+let cl = reqwest::blocking::Client::new();
+let res = cl.post("http://httpbin.org/post").body("the exact body that is sent").send().unwrap();
+ println!("Status: {}", res.status());
 }
